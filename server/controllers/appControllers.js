5442b1054eda8e0af71bc2216d7cc62039e9efa1
Adarsh01208 <adarshsingh01209@gmail.com>
2023-09-21 23:23:36 +0530

    ..

--- server/controllers/appControllers.js
+++ server/controllers/appControllers.js
@@ -1,7 +1,46 @@
+const nodemailer = require('nodemailer');
 
 
-const signup = (req , res) => {
-    res.status(200).json('Signup successfull');
+const signup = async (req , res) => {
+
+    let  testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: testAccount.user,
+          pass: testAccount.pass
+        }
+      });
+
+    let message = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    }
+
+    transporter.sendMail(message).then(info => {
+        return res.status(200).json({
+            message: 'Signup successfull',
+            info: info.messageId,
+            preview: nodemailer.getTestMessageUrl(info)
+        });
+
+        }).catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                message: 'Signup failed',
+                error: err
+            });
+        });
+
+  
+  // res.status(200).json('Signup successfull');
 }
 
 const getbill = (req , res) => {
